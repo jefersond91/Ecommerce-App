da10fd296d1874f2f22c7ce4f406f3c6a8bfbdc0
Jeferson Mesa <jefersondmj21@gmail.com>
2022-06-12 10:43:47 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Home, Purchases, Login, NewsDetail } from "./pages";
+import "./App.css";
+import { HashRouter, Route, Routes} from 'react-router-dom'
+import { Container } from "react-bootstrap";
+import {LoadingScreen} from "./components";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <HashRouter>
+            <Container>
+                <LoadingScreen />
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/news/:id" element={<NewsDetail />}/>
+                    <Route path="/purchases" element={<Purchases />}/>
+                    <Route path="/Login" element={<Login />}/>
+                </Routes>
+            </Container>
+        </HashRouter>
+    );
 }
 
 export default App;
